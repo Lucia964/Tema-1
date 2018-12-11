cab2a2056679f570921fc169c27937047d7910c9
DESKTOP-IEE8O42\Ana Maria <ana.patrascu96@yahoo.com>
2018-12-11 15:01:51 +0200

    GuessTheNumberGame

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -10,6 +10,35 @@ namespace GuessTheNumber
     {
         static void Main(string[] args)
         {
+
+            // Random rnd = new Random();
+            // int r = rnd.Next(0, 101);
+
+            Random rng = new Random();
+            int r = rng.Next(0, 100);
+            Console.WriteLine();
+
+            Console.WriteLine("Guess the number!");
+            int x;
+
+            do
+            {
+                Console.WriteLine("Enter a number");
+                string str = Console.ReadLine();
+                x = int.Parse(str);
+                if (x < r)
+                {
+                    Console.WriteLine("The number is too small!!!");
+                }
+                else
+                {
+                    Console.WriteLine("The number in too big!!!");
+                }
+
+            } while (x != r);
+            Console.WriteLine("You guess the number corectly! :)",x);
+
+
         }
     }
 }
